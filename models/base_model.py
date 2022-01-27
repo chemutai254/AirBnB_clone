87697f25c258b874104f168767f7b1730f8d12f6
chemutai254 <nancy.chemutai254@gmail.com>
2022-01-27 01:33:26 -0800

    base model

---
+++ models/base_model.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# Base model
+
+"""
+This class defines all common attributes/methods for other classes
+"""
+
+
+class BaseModel:
+    """Represents the parent model"""
+
+    def __init__(self, *args, **kwargs):
+        """Initialize the base model"""
+
